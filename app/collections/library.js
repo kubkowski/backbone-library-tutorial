b13a7aa8169e70a366ef77a66776cdbb82920466
kubkowski <jakub.opara@gmail.com>
2014-10-30 11:49:57 +0100

    Added Library Collection and Views

--- app/collections/library.js
+++ app/collections/library.js
@@ -1,1 +1,5 @@
-library.js
+var app = app || {};
+
+app.Library = Backbone.Collection.extend({
+	model: app.Book
+});
--- app/models/book.js
+++ app/models/book.js
@@ -1,1 +1,12 @@
-S
+var app = app || {};
+
+app.Book = Backbone.Model.extend({
+	defaults: {
+		coverImage: 'img/placeholder.png',
+		title: 'No title',
+		author: 'Unknown',
+		releaseDate: 'Unknown',
+		keywords: 'None'
+	}
+});
+
